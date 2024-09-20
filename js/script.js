7af7e0d7beebc1e26d294f7f9d197819a8454afd
Mohcen Benizza <mohcen.benizza.bouzid.al@iespoligonosur.org>
2024-09-20 14:37:58 +0200

    qweqwsdas

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function area(){
+
+    var radio=prompt("Introducen el area");
+
+    var area= 4*Math.PI*radio**2;
+    var volumen= (4/3)*Math.PI*radio**3;
+    alert("El area es "+area);
+    alert("EL volumen es "+volumen);
+}
+
+area();
+
+
+    
