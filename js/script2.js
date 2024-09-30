62dc13097902afaf6f2b35dd8a0ad8e16a4dbd2a
Mohcen Benizza <mohcen.benizza.bouzid.al@iespoligonosur.org>
2024-09-30 10:29:34 +0200

    Ejercicio 2

---
+++ js/script2.js
@@ -0,0 +1,66 @@
+
+/*//1
+
+function contar(){
+        var nombre=prompt("Introduce un nombre")
+        var contador=0;
+        var contador2=0;
+        var contado3=0;
+        while(nombre!="ULTIMO" &&  isNaN(nombre)){
+
+            if(nombre.charAt(0)=="C"){
+                contador2++;
+            }
+
+            if(nombre.search("ñ")){
+                contado3++;
+            }
+
+
+            contador++;
+            var nombre=prompt("Introduce otro nombre")
+
+        }
+
+        alert("Comienzan por C "+contador2+" y comienza por ñ "+contado3)
+}
+
+contar()
+
+*/
+function empiezan(){
+        var frase=prompt("Introduce una palabra que analizar ");
+
+        var contador=1;
+        var contador2=0;
+
+        if(frase.charAt(i+1)=="A"){
+            contador2++;
+        }
+
+        for(var i=0;i<frase.length;i++){
+            if(frase.charAt(i)==" "){
+                contador++;
+
+                if(frase.charAt(i+1)=="A"){
+                    contador2++;
+                }
+            }
+
+           
+        }
+        alert("Tiene tantas palabras "+contador)
+        
+        alert(contador2)
+           
+        
+
+    
+
+    
+           
+           
+      
+
+}
+empiezan()
