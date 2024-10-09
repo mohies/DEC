0d251339987a41f0322fd7fe7dfaa1aa10cbc7bb
Mohcen Benizza <mohcen.benizza.bouzid.al@iespoligonosur.org>
2024-10-09 12:46:09 +0200

    BOM

---
+++ js/script4.js
@@ -0,0 +1,13 @@
+/*//BOM
+//1
+var ventana = window.open();
+
+ventana.document.write("<p>Dame esto</p>");
+
+for (var v in window.navigator){
+    ventana.document.write("<pre>" + v + ": " + window.navigator[v] + "</pre>");
+}
+
+*/
+
+//2
