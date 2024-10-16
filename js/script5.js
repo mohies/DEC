ca84b9d0a33484e1c194cb20d92193dea9032d53
mohies <166145104+mohies@users.noreply.github.com>
2024-10-16 19:21:32 +0200

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -1,5 +1,4 @@
-
-//3
+/*//3
 let aN = [123, 4567, 89, 101112]; 
 
 function numCifras() {
@@ -19,8 +18,38 @@ let numCifras2 = (...a) => {
     console.log(contador);
 };
 numCifras2(...aN);
+*/
 
+//6
 
+let aN = [];
+let num = parseInt(prompt("Introduce un número"));
+
+while (!isNaN(num)) {
+    aN.push(num);  
+    num = parseInt(prompt("Introduce otro número (o algo no numérico para salir)"));
+}
+
+function suma(){
+    var suma=0;
+    for(let i in arguments){
+        suma=suma+arguments[i];
+    }
+    console.log("La suma "+ suma)
+}
+
+suma(...aN)
+
+let suma2=(a)=>{
+    var suma=0;
+    for(let i in a){
+        suma=suma+a[i];
+    }
+    console.log("La suma "+ suma)
+
+}
+
+suma2(aN)
 
 
 
