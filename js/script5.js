84020ce27e3311d925b8e1e4942c5c9bc917d272
mohies <166145104+mohies@users.noreply.github.com>
2024-10-21 07:55:59 +0200

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -75,16 +75,12 @@ console.log(uneArrays([1, 2, 3], [4, 5, 6], [7, 8]));
 //9 parte 4 
 
 let sumaArgPares=(...a)=>{
-    var suma=0;
-    for(let i in a){
-        if(a[i]%2===0){
-            suma=suma+a[i];
-        }
-    }
-    return suma;
+    let suma=0;
+     a.filter((valor)=> valor %2 ==0).forEach((valor)=> suma=suma+valor)
+     return suma;
 }
 
-console.log(sumaArgPares(1,2,6))
+console.log(sumaArgPares(1,2,6,8))
 
 //EJERICCIO 17
 var array= [];
@@ -104,22 +100,22 @@ document.getElementById("abrir"),addEventListener("click",()=>{
     ventana.document.write("<h1>La cantidad de palabras que hay en el array son "+array.length+"</h1>")
     ventana.document.write("<h1>LArray ordenado alfabeticamente "+array.sort()+"</h1>")
 
-})*/
+})
 
 //19
-    var array2 = [];
-    var datos = prompt("Introduce lo siguiente [Nombre, Apellido1, Apellido2, DNI, fecha Nacimiento]");
+var array2 = [];
+var datos = prompt("Introduce lo siguiente [Nombre, Apellido1, Apellido2, DNI, fecha Nacimiento]");
 
-    while (datos!=="") {
-        var array = datos.split(",");
-        array2.push(array);  
-        datos = prompt("Introduce otros datos [Nombre, Apellido1, Apellido2, DNI, fecha Nacimiento]");
-    }
+while (datos!=="") {
+    var array = datos.split(",");
+    array2.push(array);  
+    datos = prompt("Introduce otros datos [Nombre, Apellido1, Apellido2, DNI, fecha Nacimiento]");
+}
 
-    console.log(array2);
+console.log(array2);
 
 var array2=[];
-array2.push(["Juan", "Pérez", "Gómez", "12345678A", "01/01/1990"]);
+array2.push(["Juan", "Pérez", "Gómez", "12345678A", "01/01/2007"]);
 array2.push(["Ana", "Pérez", "Gómez", "87654321B", "02/02/1992"]);
 array2.push(["Luis", "García", "Fernández", "23456789C", "03/03/1988"]);
 array2.push(["María", "Rodríguez", "Sánchez", "34567890D", "04/04/1995"]);
@@ -127,24 +123,129 @@ array2.push(["Carlos", "Martínez", "Torres", "45678901E", "05/05/1985"]);
 array2.push(["Elena", "Pérez", "González", "56789012F", "06/06/1993"]);
 array2.push(["José", "Hernández", "Morales", "67890123G", "07/07/1980"]);
 console.log(array2)
+function mayordeEdad(ano){
+    let f = new Date()
+    let year= f.getFullYear();
+    return array2.filter((persona)=>(year-Number(persona[4].split("/")[2]))>=ano)
+}
+function menorEdad(ano){
+    let f = new Date()
+    let year= f.getFullYear();
+    return array2.filter((persona)=>(year-Number(persona[4].split("/")[2]))<ano)
+}
+/*function modificaDatos(){
+    let dni=prompt("Di el dni de la persona que quieres modificar");
+    let dato = prompt("¿Qué dato quieres modificar? (nombre, apellido1, apellido2, dni, fecha)").toLowerCase();
+    return array2.forEach((persona)=>{
+        if(persona[3]==dni){
+            let nuevoDato = prompt(`Introduce el nuevo valor para: ` + dato);
+            
+            // Modificar el campo adecuado según el dato que elijas
+            switch(dato) {
+                case 'nombre':
+                    persona[0] = nuevoDato;
+                    break;
+                case 'apellido1':
+                    persona[1] = nuevoDato;
+                    break;
+                case 'apellido2':
+                    persona[2] = nuevoDato;
+                    break;
+                case 'dni':
+                    persona[3] = nuevoDato;
+                    break;
+                case 'fecha':
+                    persona[4] = nuevoDato;
+                    break;
+                default:
+                    alert("Dato no válido");
+        }
+    }
+       
+    })
+}
 function buscarPorDNI(dni) {
-        return array2.find((persona)=> persona[3]=== dni); //mejor hacerlo con find esta porque te encuentra el priemer elemento 
-    }
+    return array2.find((persona)=> persona[3]=== dni); //mejor hacerlo con find esta porque te encuentra el priemer elemento 
+}
 function buscarPorApellidos(apellido1, apellido2) {
-        return array2.filter( (persona)=>persona[1] === apellido1 && persona[2] === apellido2);
-    }
+    return array2.filter( (persona)=>{
+        let edad = menorEdad(18).includes(persona);
+        return persona[1] === apellido1 && persona[2] === apellido2 && edad;
+    });
+}
 function buscarPorFecha(fecha) {
-        return array2.find((persona)=> persona[4]=== fecha);
-    }
+    return array2.find((persona)=> persona[4]=== fecha);
+}
 
+function eliminarPersona() {
+    let dni = prompt("Di el dni de la persona que quieres eliminar");
+    
+  
+    let personaAEliminar = array2.find((persona) => persona[3] === dni);
+    
+   
+    if (personaAEliminar) {
+        let confirmacion = confirm("¿Seguro que deseas eliminar a " + personaAEliminar[0] + " con DNI " + dni + "?");
+        
+       
+        if (confirmacion) {
+            array2 = array2.filter((persona) => persona[3] !== dni);
+            console.log(array2); 
+        }
+    } else {
+        alert("Persona no encontrada");
+    }
+}
 
 var dni = buscarPorDNI("67890123G");
 var apellidos = buscarPorApellidos("Pérez", "Gómez");
 var f=buscarPorFecha("04/04/1995")
-    
+var p=mayordeEdad(18)
+eliminarPersona()
+console.log(array2)
 console.log(dni);
 console.log(apellidos);
 console.log(f);
 
+//11
+function extraerNumerosYSumar(cadena) {
+  
+    const numeros = cadena.match(/\d+/g); 
+    if (numeros) {
+ 
+        const numerosEnteros = numeros.map(Number);
 
+        const suma = numerosEnteros.reduce((acc, num) => acc + num, 0);
 
+        console.log("Números encontrados:", numerosEnteros);
+        console.log("Suma de los números:", suma);
+    } else {
+        console.log("No se encontraron números en la cadena.");
+    }
+}
+
+const entrada = prompt("Introduce una cadena con números:");
+extraerNumerosYSumar(entrada);*/
+//13
+
+let rellenar=()=>{
+    var n= prompt("Introduce numero")
+
+    while(isNaN(n)){
+        var n= prompt("Introduce numero")
+
+    }
+
+    var a = [];
+for(let i=0;i<n;i++){
+    var m=[]
+    for(let j=0;j<n;j++){
+        m.push(Math.floor(Math.random() * 100)); // Añadimos un número ale
+    }
+    a.push(m)
+}
+console.log(a)
+
+}
+
+rellenar()
