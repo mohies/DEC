674135e0715daf17db3c01c7ad314b50b4db4030
mohies <166145104+mohies@users.noreply.github.com>
2024-10-29 19:33:22 +0100

    Update script5.js
    Hecho el 28 pero preguntar

--- js/script5.js
+++ js/script5.js
@@ -447,19 +447,60 @@ class Persona {
 const per1=new Persona("34423453L","Pepe",18)
 console.log(per1.muestra())*/
 
-//Ejercicio 21 con objetos
+//Ejercicio 28
 
-class Persona {
-    constructor(dni, nombre, edad) {
+class Alumno {
+    constructor(nombre, dni, curso, nota) {
+        this.nombre = nombre;
         this.dni = dni;
-        this.nombre=nombre;
-        this.edad=edad;
+        this.curso = curso;
+        this.nota = new Map();
     }
-    muestra(){
-        return this.nombre + " tiene dni "+this.dni+" y tiene una edad de "+ this.edad+" años"; 
+    muestra() {
+        let resultado = this.nombre + " tiene dni " + this.dni + " esta en el curso de " + this.curso;
+        for (let [clave, valor] of this.nota) {
+            resultado += " tiene en la asignatura de  " + clave + "  una nota de un " + valor;
+        }
+        return resultado;
+    }
+    agregarNota(clave, valor) {
+        this.nota.set(clave, valor);
+    }
+
+    lamedia() {
+        var media = 0;
+        for (let [clave, valor] of this.nota) {
+            media += valor;
+        }
+        var longitud = (this.nota.size)
+        var resultado = media / longitud;
+        console.log("La media es " + resultado)
+    }
+
+    mejornota() {
+        var notaMaxima = 0;
+        var mejorAsignatura = "";
+
+        for (let [clave, valor] of this.nota) {
+            if (valor >= notaMaxima) {
+                notaMaxima = valor;
+                mejorAsignatura = clave;
+            }
+        }
+
+        return "La mejor nota es de la asignatura " + mejorAsignatura + " con un " + notaMaxima;
     }
 
 }
 
-const per1=new Persona("34423453L","Pepe",18)
-console.log(per1.muestra())*/
+const al1 = new Alumno("Pepe", "23424234F", "2ºDAW")
+al1.agregarNota("DAW", 8);
+al1.agregarNota("DIW", 9);
+al1.agregarNota("DWES", 7);
+al1.agregarNota("DWEC", 6);
+al1.agregarNota("EIE", 10);
+
+
+console.log(al1.muestra());
+console.log(al1.lamedia());
+console.log(al1.mejornota());
