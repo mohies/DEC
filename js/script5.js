4e7867a5cee192a08ce4a1b672bb0fadf61c2ddc
mohies <166145104+mohies@users.noreply.github.com>
2024-11-07 18:44:22 +0100

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -4,7 +4,7 @@ let aN = [123, 4567, 89, 101112];
 function numCifras() {
     let contador = 0;
     for (let i in arguments) {
-        contador += String(parseInt[i]).length;
+        contador += String(arguments[i]).length;
     }
     console.log(contador);
 }
@@ -18,7 +18,6 @@ let numCifras2 = (...a) => {
     console.log(contador);
 };
 numCifras2(...aN);
-
 
 //6
 
